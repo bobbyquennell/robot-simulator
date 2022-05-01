8fb25fca6e4c42823533d8eed23c8a7781f4a886
Bobby Wang <bobbyquennell@gmail.com>
2022-05-01 19:32:16 +1000

    refactor unit test for MOVE

--- src/Robot/Robot.test.ts
+++ src/Robot/Robot.test.ts
@@ -1,5 +1,5 @@
 import { Robot } from './Robot';
-import { Position } from './types';
+import { Direction, Position } from './types';
 
 describe('1. A Robot executes:', () => {
   describe('PLACE', () => {
@@ -19,11 +19,19 @@ describe('1. A Robot executes:', () => {
   });
 
   describe('MOVE', () => {
-    it('should move 1 step forward in the direction of current facing', () => {
-      const sut = new Robot();
-      sut.place(0, 0, 'NORTH');
-      sut.move();
-      expect(sut.position).toEqual({ x: 0, y: 1, facing: 'NORTH' });
-    });
+    it.each([
+      [0, 0, 'NORTH', { x: 0, y: 1, facing: 'NORTH' }],
+      [0, 2, 'EAST', { x: 1, y: 2, facing: 'EAST' }],
+      [1, 2, 'WEST', { x: 0, y: 2, facing: 'WEST' }],
+      [1, 2, 'SOUTH', { x: 1, y: 1, facing: 'SOUTH' }],
+    ])(
+      'with current location: x: %d, y: %d, f: %s, should move 1 step forward to: %s',
+      (x, y, facing, expectedPosition) => {
+        const sut = new Robot();
+        sut.place(x, y, facing as Direction);
+        sut.move();
+        expect(sut.position).toEqual(expectedPosition);
+      },
+    );
   });
 });
